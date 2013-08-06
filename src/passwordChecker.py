df7fa7b2eb2563d56a4d32173292b3a4e77ea57b
tolo137 <matthewj.egan@hotmail.com>
2013-08-06 16:43:53 +1000

    Added src directory and passwordChecker.py

---
+++ src/passwordChecker.py
@@ -0,0 +1,11 @@
+## passwordChecker.py
+##
+## Written by Matthew Egan
+## Created: 6th August 2013
+## Last Revised: 6th August 2013
+##
+## For more information: https://github.com/tolo137/PasswordChecker.git
+
+class passwordChecker:
+    def __init__(self, password):
+        self.password = password
